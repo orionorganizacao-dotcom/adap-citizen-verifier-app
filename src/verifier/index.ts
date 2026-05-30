01ca103a8872941cc4d67edf7261b90989f3fbcc
Orion Organization <orionorganizacao@gmail.com>
2026-05-30 18:25:34 -0300

    Create index.ts to export verifier modules

---
+++ src/verifier/index.ts
@@ -0,0 +1,7 @@
+export * from "./types";
+export * from "./parseEnvelope";
+export * from "./validateSchema";
+export * from "./compareExplanation";
+export * from "./analyzeDependencies";
+export * from "./generateResult";
+export * from "./runVerification";
