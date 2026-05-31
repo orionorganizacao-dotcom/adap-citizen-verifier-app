6b463257d3543fd50247c2dce4025778af2cd9ac
Orion Organization <orionorganizacao@gmail.com>
2026-05-30 23:36:41 -0300

    Fix verifier barrel exports

--- src/verifier/index.ts
+++ src/verifier/index.ts
@@ -1,58 +1,36 @@
-export type {
-  ResultCardAction,
-  ResultCardSection,
-  ResultCardTone,
-  ResultCardViewModel
-} from "./ResultCard";
+export {
+  parseEnvelope
+} from "./parseEnvelope";
 
 export {
-  buildResultCard,
-  formatDependencyWarning,
-  formatDependencyWarnings,
-  formatMismatches,
-  getActionDescription,
-  getActionLabel,
-  getPrimaryAction,
-  getResultAriaLabel,
-  getResultBadge,
-  getResultHeadline,
-  getResultSummary,
-  getResultTitle,
-  getResultTone,
-  getSecondaryActions,
-  isCriticalResult,
-  shouldShowEscalationHint
-} from "./ResultCard";
-
-export type {
-  PlainTextReportOptions
-} from "./formatReport";
+  validateSchema
+} from "./validateSchema";
 
 export {
-  appendMarkdownList,
-  buildSafeFilename,
-  createMarkdownReportBlob,
-  createTextReportBlob,
-  formatCoreStatuses,
-  formatDoesNotProve,
-  formatEscalationText,
-  formatFooter,
-  formatHeader,
-  formatInlineList,
-  formatLimitationNotice,
-  formatListSection,
-  formatMachineReadableHint,
-  formatMarkdownReport,
-  formatMatchedFields,
-  formatMismatchSection,
-  formatMissingFields,
-  formatOptionalIssueSummary,
-  formatPlainTextReport,
-  formatRecommendedNextSteps,
-  formatRecommendedStep,
-  formatResultSummary,
-  formatShortReport,
-  formatTechnicalDetails,
-  getMarkdownReportFilename,
-  getPlainTextReportFilename
-} from "./formatReport";
+  compareExplanation
+} from "./compareExplanation";
+
+export {
+  analyzeDependencies
+} from "./analyzeDependencies";
+
+export {
+  generateResult
+} from "./generateResult";
+
+export {
+  runFixtureVerification,
+  runVerification
+} from "./runVerification";
+
+export type {
+  VerificationResult,
+  ExportableVerificationResult,
+  RunVerificationInput,
+  RunVerificationOutput,
+  ResultLevel,
+  FieldMismatch,
+  DependencyWarning,
+  RecommendedNextStep,
+  DoesNotProve
+} from "./types";
