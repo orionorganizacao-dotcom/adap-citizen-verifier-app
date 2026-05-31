57b74b83e6833c4841e6938051bc099c13d7fd57
Orion Organization <orionorganizacao@gmail.com>
2026-05-30 23:40:59 -0300

    Fix UI barrel exports

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -1,2 +1,18 @@
-export * from "./ResultCard";
-export * from "./formatReport";
+export {
+  buildResultCard,
+  getResultAriaLabel,
+  getResultBadge,
+  getResultHeadline,
+  getResultTone,
+  getResultTitle,
+  isCriticalResult,
+  shouldShowEscalationHint
+} from "./ResultCard";
+
+export {
+  formatMarkdownReport,
+  formatPlainTextReport,
+  formatShortReport,
+  getMarkdownReportFilename,
+  getPlainTextReportFilename
+} from "./formatReport";
