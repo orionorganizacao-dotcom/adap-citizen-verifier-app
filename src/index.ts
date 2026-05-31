7ece7985b076725a041f9464fbf5e1b319338897
Orion Organization <orionorganizacao@gmail.com>
2026-05-30 23:53:33 -0300

    Fix root barrel duplicate exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,25 @@
 export * from "./verifier";
 export * from "./ui";
-export * from "./app";
+
+export {
+  allDemoCasesPassed,
+  createDemoCaseInput,
+  findDemoOutput,
+  formatDemoSuiteSummary,
+  getDemoCaseDescription,
+  getDemoCaseOrder,
+  getDemoCaseTitle,
+  getDemoOutputSummary,
+  getExpectedLevelForDemoCase,
+  runDemoCase,
+  runDemoCases,
+  runDemoSuite,
+  summarizeDemoResults
+} from "./app";
+
+export type {
+  DemoCaseInput,
+  DemoCaseName,
+  DemoCaseOutput,
+  DemoRunnerSummary
+} from "./app";
